21a04e5f0a43d3cea71b4e394ddfa4905890777f
Kim Jee Min <kingjeemin@gmail.com>
2016-05-23 01:14:26 +0900

    안드로이드 아두이노 블루투스 통신 확인
    페어링 후 안드로이드 어플리케이션 버튼과 LED 연동되는 것 확인.
    TX D2, RX D3.
    LED1 D7
    LED2 D8

---
+++ BluetoothOrder.ino
@@ -0,0 +1,89 @@
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+
+void setup()
+{
+  // Open serial communications and wait for port to open:
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  Serial.println("Goodnight moon!");
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+
+}
+
+char buffer[64];
+int buffer_position = 0; 
+
+void buffer_clear()
+{
+  int i= 0; 
+  for(i = 0; i< 64; i++)
+    buffer[i] = 0x0;
+  buffer_position = 0; 
+}
+void loop() // run over and over
+{
+  if (mySerial.available()){
+    char c = mySerial.read();
+    if(c == '\n'){
+      Serial.println(buffer);
+      switch(buffer[0]){
+        case 'K':
+         {
+            switch(buffer[1]) {
+              case '0':
+                Serial.print("LED 0:");
+                Serial.println(buffer[5]);
+                if(buffer[5] == '0') 
+                  digitalWrite(8, LOW);
+                else 
+                  digitalWrite(8, HIGH);
+              break;
+              default:
+              break;             
+            }
+            mySerial.write("R:OK\n");
+          }
+        break;
+        case 'L':
+          {
+            switch(buffer[1]) {
+              case '0':
+                Serial.print("LED 1:");
+                Serial.println(buffer[5]);
+                if(buffer[5] == '0') 
+                  digitalWrite(7, LOW);
+                else 
+                  digitalWrite(7, HIGH);
+              break;
+              default:
+              break;             
+            }
+            mySerial.write("R:OK\n");
+          }
+        break;
+        default:
+          mySerial.write("R:FAIL\n");
+        break;
+      }
+      buffer_clear(); 
+    } else {      
+      buffer[buffer_position++] = c; 
+    }
+  }
+  
+  if (Serial.available()){
+    mySerial.write(Serial.read());
+  }
+}
+
+
+
